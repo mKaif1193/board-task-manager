26b615071db537dbb19ee3d098921a013f611ab7
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-21 12:05:33 +0500

    Final Deployment

--- hooks/useSelectionBounds.ts
+++ hooks/useSelectionBounds.ts
@@ -45,7 +45,7 @@ export const useSelectionBounds = () => {
   const selection = useSelf((me) => me.presence.selection);
 
   return useStorage((root) => {
-    const selectedLayers = selection
+    const selectedLayers = selection!
       .map((layerId) => root.layers.get(layerId)!)
       .filter(Boolean);
 
